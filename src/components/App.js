dee3a7492e0de94f57c36794d86611b1cee83753
Jenna Palmer <jcpalmer108@gmail.com>
2023-01-14 15:26:09 -0800

    basic autocomplete working

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,37 @@
-import React, { Component } from 'react'
+import React, { useState } from 'react'
+import { Autocomplete, TextField, Button } from '@mui/material';
+import symbols from '../content/data';
 import './App.css';
 
-class App extends Component {
-  render() {
-    return <div>nice</div>
-  }
+function App () {
+  const [selected, setSelected] = useState([])
+  const [displayInterpretation, setDisplayInterpretation] = useState(false)
+
+  return (
+    <div className="dream-interpreter">
+      <h1>Dream Interpreter</h1>
+      <p>Select various symbols that you may have seen in your dream, and let our engine combile our dream symbol data and tell you what your dream symbols really mean.</p>
+      <Autocomplete
+        multiple
+        id="tags-outlined"
+        options={symbols}
+        getOptionLabel={(item) => item.label}
+        filterSelectedOptions
+        value={selected}
+        disabled={displayInterpretation}
+        onChange={(event, value) => {setSelected(value)}}
+        renderInput={(params) => (
+          <TextField
+            {...params}
+            label="Dream Symbols"
+            placeholder="Choose your dream symbols..."
+          />
+        )}
+      />
+      <Button variant="contained" onClick={() => { setDisplayInterpretation(true)}}>Interpret Dream </Button>
+      
+    </div>
+  )
 }
 
 export default App;
--- src/content/data.js
+++ src/content/data.js
@@ -1,7 +1,32 @@
-const symbols = new Map([
-  [
-    "A", { default: 'hi' }
-  ]
-])
+const symbols = [
+  {
+    label: "Lecture",
+    meaning: "To dream that you are giving a lecture suggests that you are becoming somewhat of a bore. You need to improve your communications skills. Alternatively, your dream may serve as a continuation of your intellectual thinking, carried over from your waking hours.",
+    specifics: [
+      {
+        label: "Listening to a lecture",
+        additional: "To dream that you are listening to a lecture indicates that you need to do some research and weigh out your options before making a decision on some matter."
+      }
+    ]
+  },
+  {
+    label: "Queue",
+    redirect: "Line",
+  },
+  {
+    label: "Line",
+    meaning: "To see a line in your dream symbolizes duality, limits, boundaries and rules. It also relates to movement or non-movement. To dream that you are crossing a line suggests that you are overstepping your boundaries or that you are moving beyond the limits in some area of your life.",
+    specifics: [
+      {
+        label: "Line of people or items",
+        additional: "To see a line of people or objects indicates that you need to be more aware of some situation or relationship."
+      },
+      {
+        label: "Standing in line",
+        additional: "To dream that you are standing in line represents your need for patience. You need to learn to wait for something and not always have it right away."
+      }
+    ]
+  }
+]
 
 export default symbols
