44ceb40f1d43aac5deadcd12831f2f7738eba9ab
Jenna Palmer <jcpalmer108@gmail.com>
2023-01-14 17:43:37 -0800

    redid work in class based react and got the autocomplete component to work

--- src/components/App.js
+++ src/components/App.js
@@ -1,37 +1,41 @@
-import React, { useState } from 'react'
-import { Autocomplete, TextField, Button } from '@mui/material';
+import React, { Component } from 'react';
+import { Autocomplete, Button, TextField } from '@mui/material';
 import symbols from '../content/data';
 import './App.css';
 
-function App () {
-  const [selected, setSelected] = useState([])
-  const [displayInterpretation, setDisplayInterpretation] = useState(false)
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { 
+      chosenSymbols: [],
+      selected: {},
+    };
+  }
 
-  return (
-    <div className="dream-interpreter">
-      <h1>Dream Interpreter</h1>
-      <p>Select various symbols that you may have seen in your dream, and let our engine combile our dream symbol data and tell you what your dream symbols really mean.</p>
-      <Autocomplete
-        multiple
-        id="tags-outlined"
-        options={symbols}
-        getOptionLabel={(item) => item.label}
-        filterSelectedOptions
-        value={selected}
-        disabled={displayInterpretation}
-        onChange={(event, value) => {setSelected(value)}}
-        renderInput={(params) => (
-          <TextField
-            {...params}
-            label="Dream Symbols"
-            placeholder="Choose your dream symbols..."
+  render () {
+    return (
+      <div className="dream-interpreter">
+        <h1>Dream Interpreter</h1>
+        <p>Select various symbols that you may have seen in your dream, and let our engine combile our dream symbol data and tell you what your dream symbols really mean.</p>
+        <div className='symbol-text-field'>
+          <Autocomplete
+            disablePortal
+            id="symbol"
+            options={symbols}
+            sx={{ width: '100%', marginRight: '10px'}}
+            renderInput={(params) => <TextField {...params} label="Dream Symbols" />}
+            onChange={(event, value) => {
+              console.log(event, value)
+              this.setState({ ...this.state, selected: value})
+            }}
           />
-        )}
-      />
-      <Button variant="contained" onClick={() => { setDisplayInterpretation(true)}}>Interpret Dream </Button>
-      
-    </div>
-  )
+          <Button variant="contained" sx={{ width: 100 }}>Add</Button>
+        </div>
+        <div>{JSON.stringify(this.state.selected)}</div>
+      </div>
+    )
+  
+  }
 }
 
 export default App;
