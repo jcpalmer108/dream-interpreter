7c4c61f21540fc2673add36b1339d3c9d355cd5f
Jenna Palmer <jcpalmer108@gmail.com>
2023-01-14 19:03:01 -0800

    added basic radios

--- src/components/SpecificsDialog.js
+++ src/components/SpecificsDialog.js
@@ -1,5 +1,6 @@
 import React from 'react';
-import { Dialog, DialogTitle, DialogActions, Button, DialogContent } from '@mui/material';
+import { Dialog, DialogTitle, DialogActions, Button, FormControlLabel, Radio, DialogContent, FormControl, RadioGroup } from '@mui/material';
+import './SpecificsDialog.css';
 
 function SpecificsDialog (props) {
   const { handleClose, selected, open } = props;
@@ -9,13 +10,27 @@ function SpecificsDialog (props) {
   return (
     <Dialog onClose={handleClose} open={open}>
       <DialogTitle>Let's talk about your choice more...</DialogTitle>
-      <DialogContent>When thinking about the symbol <strong>{getLabel()}</strong>, did any of these specific incidents come up?</DialogContent>
+      <DialogContent>When thinking about the symbol <strong>{getLabel()}</strong> in your dream, did any of these scenarios come up?</DialogContent>
+      <FormControl className="radio-container">
+        <RadioGroup
+          aria-labelledby="demo-radio-buttons-group-label"
+          defaultValue="female"
+          name="radio-buttons-group"
+        >
+          <FormControlLabel value="female" control={<Radio />} label="Female" />
+          <FormControlLabel value="male" control={<Radio />} label="Male" />
+          <FormControlLabel value="other" control={<Radio />} label="Other" />
+        </RadioGroup>
+     </FormControl>
+
+
+      
       <DialogActions>
-          <Button onClick={handleClose}>Exit</Button>
-          <Button onClick={handleClose} autoFocus>
-            Submit
-          </Button>
-        </DialogActions>
+        <Button onClick={handleClose}>Exit</Button>
+        <Button onClick={handleClose} variant="contained" autoFocus>
+          Submit
+        </Button>
+      </DialogActions>
 
     </Dialog>
   );
